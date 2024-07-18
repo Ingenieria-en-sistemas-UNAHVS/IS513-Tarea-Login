ba88b470dd2377df81de60b70b8aa577ac689aa3
Andres Leiva <andresleivaamaya@gmail.com>
2024-07-18 09:48:11 -0600

    Homepage design

--- tarea_login/lib/home_page.dart
+++ tarea_login/lib/home_page.dart
@@ -4,8 +4,12 @@ class HomePage extends StatelessWidget {
   
   const HomePage({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
+    final datos = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -14,6 +18,37 @@ class HomePage extends StatelessWidget {
           fontSize: 30,
         ),
         title: const Text('Inicio'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //Logo
+              SizedBox(
+                height: 200,
+                child: Image.asset('assets/images/unah.png'),
+              ),
+              //Boton de cerrar sesion
+              Card(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: Text('Bienvenido ${datos['nombre']}',style: TextStyle(fontSize: 25, color: Colors.blueAccent),),
+                    ),
+                    ListTile(
+                      title: const Text('Correo:',style: TextStyle(fontSize: 25, color: Colors.blueAccent),),
+                      subtitle: Text(datos['correo']),
+                    ),
+                    ListTile(
+                      title: const Text('Telefono:',style: TextStyle(fontSize: 25, color: Colors.blueAccent),),
+                      subtitle: Text(datos['telefono']),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
         )
     );
   }
